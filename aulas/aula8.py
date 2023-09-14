d7bffcfd838493906182e2b10d66a9b6f2369466
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-14 16:39:45 -0300

    aula8 criada

---
+++ aulas/aula8.py
@@ -0,0 +1,13 @@
+nome = 'Emerson'
+sobrenome = 'Thiago'
+idade = 20
+ano_nascimento = 2003
+maior_de_idade = 20 >= 18
+altura_metros = 1.65
+
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('E maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metros)
