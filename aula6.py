6f832169f3bd85aab908a23bb15c1a8cf944a87b
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-14 15:46:47 -0300

    aula6 finalizada

--- aula6.py
+++ aula6.py
@@ -6,10 +6,15 @@
 print(1 + 1) #soma
 print('a' + 'b') #concatenação
 
+# int float
 print('1', type('1'))
 print(int('1'), type(int('1')))
 print(int('1') + 2)
 print(type(float('1') + 1))
 
+# bool
 print(bool('')) # vazia == False
 print(bool(' ')) # com valor == True
+
+# string
+print(str(11) + 'b')
