1d4bb59e5f694ee1fefc1bd836bb42d574c107a0
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-14 15:40:08 -0300

    Update aula6.py

--- aula6.py
+++ aula6.py
@@ -3,3 +3,13 @@
 # é o ato de converter um tipo em outro
 # tipos imutáveis e primitivos:
 # str, int, float, bool
+print(1 + 1) #soma
+print('a' + 'b') #concatenação
+
+print('1', type('1'))
+print(int('1'), type(int('1')))
+print(int('1') + 2)
+print(type(float('1') + 1))
+
+print(bool('')) # vazia == False
+print(bool(' ')) # com valor == True
