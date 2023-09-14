e617dd1499f6386b3f1795ba6533837119d460a6
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-14 14:07:35 -0300

    aula3 criada

--- aula2.py
+++ aula2.py
@@ -1,4 +1,7 @@
-print(12, 34, 1011, sep='-')
-print(12, 34, sep='-')
+# \r\n => CRLF
+# \n => LF
+print(12, 34, 1011, sep='-', end='\n##')
+print(12, 34, sep='-', end='\n')
+print(9, 10, sep='', end='\n')
 
 
