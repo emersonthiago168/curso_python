082462a9dfcaa84d5470bc5618834db3b5b30283
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-14 13:56:32 -0300

    atualizado

--- aula1.py
+++ aula1.py
@@ -1,4 +1,18 @@
+"""
+(DocString != Comentário. Mas é utilizado como se fosse
+pra mais de uma linha.)
+Escrever o que eu 
+quiser
+
+sdamdslka
+"""
+
+'''
+(Também é DocString, mas com aspas simples)
+Usar para escrever suas notas
+'''
+
 # Permite escrever um comentário
 print(123) # Na frente
 # Abaixo
-print(123)
+print(456)
