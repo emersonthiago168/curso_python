69dd899a926640d389461427aaa6e6a2d2edb077
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-15 16:03:56 -0300

    Operador lógico "not"

--- aulas/aula23.py
+++ aulas/aula23.py
@@ -1,10 +1,7 @@
-# Operador lógico "not" 
+# Operador lógico "not"
 # Usado para inverter expressões
 # not True = False
 # not False = True
-senha = input('Senha: ')
-
-if senha == '123456':
-    print('Entrou')
-else:
-    print('Senha incorreta')
+# senha = input('Senha: ')
+print(not True)  # False
+print(not False)  # True
