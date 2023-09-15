28cf47d8099c4e818ee5c39b848f49695b31edca
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-15 14:07:49 -0300

    Exercício de programação com if e comparação

---
+++ aulas/aula20.py
@@ -0,0 +1,10 @@
+primeiro_valor = input('Digite um valor: ')
+segundo_valor = input('Digite outro valor: ')
+
+if primeiro_valor > segundo_valor:
+    print(f'O {primeiro_valor=} é maior que o {segundo_valor=} ')
+else:
+    print(f'O {segundo_valor=} é maior que o {primeiro_valor=}')
+
+
+# variavel = é maior que o segundo variavel
