96a57c2dcf850a0bb20925658c5cbbca5295e177
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-15 18:26:16 -0300

    Exercício: teste seu conhecimento até aqui

--- aulas/aula28.py
+++ aulas/aula28.py
@@ -18,7 +18,7 @@ nome = input('Qual é o seu nome? ')
 idade = input('Qual é a sua idade? ')
 
 
-if (nome and idade) not in ' ':
+if nome and idade:
     print(f'Seu nome é {nome}')
     print(f'Seu nome invertido é {nome[::-1]}')
     if ' ' in nome:
