ff5ca61d453e52351b28874b8ddec1a99544a992
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-15 15:23:17 -0300

    Operador Lógico "or"

--- aulas/aula22.py
+++ aulas/aula22.py
@@ -11,7 +11,6 @@
 
 # entrada = input('[E]ntrar [S]air: ')
 # senha_digitada = input('Senha: ')
-
 # senha_permitida = '123456'
 
 # if (entrada == 'E' or entrada == 'e') and senha_digitada == senha_permitida:
@@ -20,3 +19,5 @@
 #      print('Sair')
 
 # Avaliação de curto circuito
+senha = input('Senha: ') or 'Sem senha'
+print(senha)
