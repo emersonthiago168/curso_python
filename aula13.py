96c9f793e06b37b15fbd948c627598748d9f1ba0
Emerson Thiago <142616585+emersonthiago168@users.noreply.github.com>
2023-09-15 10:04:07 -0300

    Uma introdução às f-strings (formatação de strings)

---
+++ aula13.py
@@ -0,0 +1,10 @@
+nome = 'Emerson Thiago'
+altura = 1.65
+peso = 61
+imc = peso / (altura ** 2)
+
+# f-strings
+linha_1 = f'{nome} tem {altura} de altura, pesa {peso} quilos e seu IMC é {imc:.2f}'
+
+print(linha_1)
+
